a36fefee3d2efc6f7e6e098ea99183fcb7b5cffc
Amber Kerawala <amber_kerawala@berkeley.edu>
2020-05-08 20:16:21 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,20 @@ require 'sinatra/base'
 class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
+    
+    get '/' do 
+      erb :'../views/super_hero'
+    end 
+    #Get data, show the initial form called super_hero in views where the user 
+    
+    post '/teams' do 
+      @team_name = params["team"]["name"]
+      @team_motto = params["team"]["motto"]
+      @team_members = params["team"]["members"]
+      erb :'../views/team'
+    end 
+    #The form should have fields for the name of a superhero team and their motto. This route responds to a post request at /teams. The form sends a post request to this route. 
+    
 
 
 end
