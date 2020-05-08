e24d4863b2788c3df84dccc975a529a3330653d2
Amber Kerawala <amber_kerawala@berkeley.edu>
2020-05-08 20:03:13 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class App < Sinatra::Base
     post '/teams' do 
       @team_name = params["team"]["name"]
       @team_motto = params["team"]["motto"]
-      @team_members = params[]
+      @team_members = params["team"]["members"]
       erb :'../views/team'
     end 
     #The form should have fields for the name of a superhero team and their motto. This route responds to a post request at /teams. The form sends a post request to this route. 
